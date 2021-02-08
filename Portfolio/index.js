13dcc2f72d63912a2f47d5009229f8ff75ee41d1
josbherdd <78630368+josbherdd@users.noreply.github.com>
2021-02-08 12:58:33 -0500

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,24 @@
+function trabajoClick(url){
+  window.open(url, '_blank');
+}
+
+const texts = ["Artist","3Dmodeling","Developer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+// (function type(){
+//     if (count === texts.length){
+//       count = 0;
+//     }
+//     currentText = texts[count];
+//     letter = currentText.slice(0, ++index);
+//     console.log(letter)
+//     document.querySelector(".typing").textContent = letter;
+//     if (letter.length === currentText.length) {
+//       count++;
+//       index = 0;
+//     }
+//     setTimeout(type, 400);
+// })();
